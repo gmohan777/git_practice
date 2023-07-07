8b1e8b3a6a72fea65794900c9984c9a46118bb93
mohan <gonuguntamohankrishna@gmail.com>
2023-07-07 12:21:42 +0530

    Practiced margins,padding and created a JS file

---
+++ index.js
@@ -0,0 +1,8 @@
+function alertm() {
+  alert("Details submitted succesfully");
+}
+
+function reverse() {
+  document.getElementById("flower").src = "/images/images2/Tree sunset.jpg";
+  document.getElementById("tree").src = "/images/images2/flower.jpg";
+}
